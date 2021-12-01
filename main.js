db575a90e54e00074c039ba1d32086a32fbca544
shreehars <88490933+shreehars@users.noreply.github.com>
2021-12-01 16:45:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ function addUser(){
     player2_name = document.getElementById("player2_name_input").value;
     localStorage.setItem("player1_name", player1_name);
     localStorage.setItem("player2_name", player2_name);
-    window.location = "game_page.html";
+    window.location = "gamepage.html";
 }
